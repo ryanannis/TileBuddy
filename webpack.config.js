a67d53a0db04a7e12b5cc37528cf9396822e0e91
Ryan Annis <tinnymint@gmail.com>
2017-01-24 21:15:09 -0800

    prod build

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: "inline-source-maps",
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
     './src/index.js'
   ],
   output: {
@@ -16,13 +13,10 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      loaders: ['react-hot', 'babel'],
+      loaders: ['babel'],
       include: path.join(__dirname, 'src')
     }]
   }
